7af88cb651bc6628dacc97986442508771793e36
Aman Maurya <aman.20223030@mnnit.ac.in>
2025-01-18 14:51:14 +0530

    1st

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config({path:"backend/config/config.env"});
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+});
