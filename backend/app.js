92dc8ecd63c121573bb908a5cf7b7d75ec559e3d
Aman Maurya <aman.20223030@mnnit.ac.in>
2025-01-18 17:26:58 +0530

    2nd

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 import express from "express";
-import dotenv from "dotenv";
 const app = express();
+import dotenv from "dotenv";
+import { connectDatabase } from "./config/dbConnect.js";
 
 dotenv.config({path:"backend/config/config.env"});
 
-app.listen(process.env.PORT,()=>{
+
+//connecting to DB
+connectDatabase();
+
+//Import all routes
+import productRoutes from "./routes/products.js";
+
+app.use("/api/v1",productRoutes);
+
+
+app.listen(process.env.PORT,() => {
     console.log(`server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
