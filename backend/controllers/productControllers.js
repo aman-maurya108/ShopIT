749ff67ebad16b1195462a16f0c591a5a271d20c
shivam9-9 <shivamdubey5163@gmail.com>
2025-03-25 00:07:10 +0530

    created routes and handler function for CRUD of Product model

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,68 @@
+import Product from '../models/product.js';
+
+
 export const getProducts = async(req,res) => {
+
+    const products = await Product.find();
+
     res.status(200).json({
-        messagae: "All Products",
+        products,
     });
 };
+
+
+export const newProduct = async(req,res) => {
+    const product = await Product.create(req.body);
+
+    res.status(200).json({
+        product,
+    });
+};
+
+export const getProductDetails = async(req,res) => {
+    const product = await Product.findById(req?.params?.id);
+
+    if(!product){
+        return res.status(404).json({
+            error: "Product not found",
+        });
+    }
+
+    res.status(200).json({
+        product,
+    });
+};
+
+export const updateProduct = async(req,res) => {
+    let product = await Product.findById(req?.params?.id);
+
+    if(!product){
+        return res.status(404).json({
+            error: "Product not found",
+        });
+    }
+
+    product = await Product.findByIdAndUpdate(req?.params?.id, req.body,{new:true});
+
+    res.status(200).json({
+        product,
+    });
+};
+
+export const deleteProduct = async(req,res) => {
+    let product = await Product.findById(req?.params?.id);
+
+    if(!product){
+        return res.status(404).json({
+            error: "Product not found",
+        });
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        success:true,
+        message:"Product deleted",
+    });
+};
+
