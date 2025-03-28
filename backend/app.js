f90c9f46c3ae3659740259c60ac7c20017996940
aaditya3456 <rajaditya59385@gmail.com>
2025-03-29 03:42:22 +0530

    Authorization and Authentication

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app =express()
 import dotenv from "dotenv"
 import { connectDatabase } from "./config/dbConnect.js";
 import errorMiddleware from "./middlewares/errors.js";
+import cookieParser  from "cookie-parser";
 //Handle Uncaught Exception
 process.on("uncaughtException",(err)=>{
     console.log(`ERRROR: ${err}`);
@@ -15,11 +16,16 @@ dotenv.config({path: "backend/config/config.env"});
 //connecting to database
 connectDatabase();
 app.use(express.json());
+app.use(cookieParser());
 
 //import all routes
 import productRoutes from "./routes/products.js";
+import authRoutes from "./routes/auth.js";
+
 
 app.use("/api/v1",productRoutes);
+app.use("/api/v1",authRoutes);
+
 
 app.use(errorMiddleware);
 
