5010c39f27f2c9ba67d8d94a461910f89c01cea6
saurabh941568 <saurabhsingh9415686158@gmail.com>
2025-03-30 00:13:10 +0530

    handle user route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { forgotPassword, loginUser, logout, registerUser,resetPassword } from "../controllers/authControllers.js";
+import { forgotPassword, getUserDetails, getUserProfile, loginUser, logout, registerUser,resetPassword, updatePassword, updateProfile,allUsers, updateUser, deleteUser } from "../controllers/authControllers.js";
 const router = express.Router();
+
+import { authorizeRoles, isAuthenticatedUser} from "../middlewares/auth.js";
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
@@ -9,6 +11,22 @@ router.route("/logout").get(logout);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
+
+
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
+router
+.route("/admin/users")
+.get(isAuthenticatedUser,authorizeRoles("admin"), allUsers);
+
+router
+.route("/admin/users/:id")
+.get(isAuthenticatedUser,authorizeRoles("admin"), getUserDetails)
+.put(isAuthenticatedUser,authorizeRoles("admin"), updateUser)
+.delete(isAuthenticatedUser,authorizeRoles("admin"), deleteUser);
 
 
 
