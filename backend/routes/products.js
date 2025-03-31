8c37ac125b6512e26061ac546b0db494981cd513
anuj-yadv <anujyadav.gzp730@gmail.com>
2025-04-01 01:03:38 +0530

    routes and controllers for reviews

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteProduct, getProductDetails, getProducts, newProduct, updateProduct } from "../controllers/productControllers.js";
+import { createProductReview, deleteProduct, deleteReview, getProductDetails, getProductReviews, getProducts, newProduct, updateProduct } from "../controllers/productControllers.js";
 import { isAuthenticatedUser ,authorizeRoles} from "../middlewares/auth.js";
 const router = express.Router();
 
@@ -10,4 +10,9 @@ router.route("/products/:id").get(getProductDetails);
 
 router.route("/admin/products/:id").put(isAuthenticatedUser, authorizeRoles('admin'),updateProduct);
 router.route("/admin/products/:id").delete(isAuthenticatedUser, authorizeRoles('admin'),deleteProduct);
+router.route("/reviews")
+.put(isAuthenticatedUser,createProductReview)
+.get(isAuthenticatedUser,getProductReviews);
+
+router.route("/admin/reviews").delete(isAuthenticatedUser,authorizeRoles("admin"),deleteReview);
 export default router;
