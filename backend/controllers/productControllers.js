ed3a7385eb9953f417d72f8482a6fd93fbb2ac99
anuj-yadv <anujyadav.gzp730@gmail.com>
2025-04-04 23:34:54 +0530

    5

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -11,7 +11,7 @@ export const getProducts = catchAsyncErrors(async(req,res)=>{
     let products = await apiFilters.query;
 
     let filteredProductsCount = products.length;
-
+   
     apiFilters.pagination(resPerPage);
 
     products = await apiFilters.query.clone();
