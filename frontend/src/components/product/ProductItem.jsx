5f55aec4488c4ed4d74ca4601c3f2375a970d36c
anuj-yadv <anujyadav.gzp730@gmail.com>
2025-04-06 15:05:15 +0530

    Adding Pagination,Search & Filters

--- frontend/src/components/product/ProductItem.jsx
+++ frontend/src/components/product/ProductItem.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {Link} from "react-router-dom"
 import ReactStars from "react-rating-stars-component";
 
-export default function ProductItem({product}) {
+export default function ProductItem({product,columnSize}) {
   return (
     <div>
-      <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+      <div className={`col-sm-12 col-md-6 col-lg-${columnSize} my-3`}>
                 <div className="card p-3 rounded">
                   <img
                     className="card-img-top mx-auto"
-                    src={product?.images[0]?.url}
+                    src={product?.images[0] ? product?.images[0]?.url : '/images/defualt_product.png'}
                     alt={product?.name}
                   />
                   <div
