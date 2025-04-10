18167d4f8f1b853ec78cd4e6046887404dbd4379
shivam9-9 <shivamdubey5163@gmail.com>
2025-04-11 01:27:50 +0530

    added cart feature

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -12,6 +12,8 @@ const Header = () => {
   const [logout] = useLazyLogoutQuery();
 
   const { user } = useSelector((state) => state.auth);
+  const { cartItems } = useSelector((state) => state.cart);
+
 
   const logoutHandler = () => {
     logout();
@@ -37,7 +39,7 @@ const Header = () => {
             Cart{" "}
           </span>
           <span className="ms-1" id="cart_count">
-            0
+            {cartItems?.length}
           </span>
         </a>
 
