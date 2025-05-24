dfaa19dc2d9bfd900543155432d00aa6c8808bbd
Aman Maurya <aman.20223030@mnnit.ac.in>
2025-05-24 10:55:19 +0530

    prepared app for deployment

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,7 +4,7 @@ import Product from "../models/product.js";
 
 const seedProducts = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/shopit-v2");
+    await mongoose.connect("mongodb+srv://amanmaurya20223030:Uz3L4A0w99B5UeSy@shopit.7oiyxqb.mongodb.net/ShopIT");
 
     await Product.deleteMany();
     console.log("Products are deleted");
